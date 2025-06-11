262c70679ba01d05a643d3ba6bbc4c13e6c9d7bc
Yashrajsinh-Kanchva <yashrajsinhkanchva7435@gmail.com>
2025-06-11 21:41:28 +0530

    Some changes in code of Q2682

--- Easy/lc2682.java
+++ Easy/lc2682.java
@@ -12,33 +12,56 @@ public class lc2682 {
 class Solution2682 {
     public int[] circularGameLosers(int n, int k) {
         // 6ms
-        Hashtable<Integer, Integer> player=new Hashtable<>();
-        for(int i=1; i<=n; i++){
-            player.put(i, 0);
-        }
-        int start=1;
-        int step=k;
-        int I=2;
+//        Hashtable<Integer, Integer> player=new Hashtable<>();
+//        for(int i=1; i<=n; i++){
+//            player.put(i, 0);
+//        }
+//        int start=1;
+//        int step=k;
+//        int I=2;
+//        while(true){
+//            int val=player.get(start);
+//            player.put(start, ++val);
+//            if(player.get(start)==2){
+//                player.remove(start);
+//                break;
+//            }
+//            start=start+step;
+//            if(start>n)
+//                start=(start % n == 0) ? n : start % n;
+//
+//            step=k*I;
+//            I++;
+//        }
+//
+//        int[] ans=new int[n-I+2];
+//        int j=0;
+//        for(int i=1; i<=n; i++){
+//            if(player.containsKey(i) && player.get(i)==0){
+//                ans[j++]=i;
+//            }
+//        }
+//        return ans;
+
+        //1ms
+        boolean[] player=new boolean[n];
+        player[0]=true;
+        int i=1;
+        int last=1;
         while(true){
-            int val=player.get(start);
-            player.put(start, ++val);
-            if(player.get(start)==2){
-                player.remove(start);
+            last= (last+(i*k))%n;
+            if(last==0) last=n;
+            if(player[last-1])
                 break;
-            }
-            start=start+step;
-            if(start>n)
-                start=(start % n == 0) ? n : start % n;
-
-            step=k*I;
-            I++;
+            i++;
+            player[last-1]=true;
         }
 
-        int[] ans=new int[n-I+2];
-        int j=0;
-        for(int i=1; i<=n; i++){
-            if(player.containsKey(i) && player.get(i)==0){
-                ans[j++]=i;
+        int ans[]= new int[n-i];
+        int ansIndex=0;
+        for(int j=0; j<n; j++){
+            if(!player[j]){
+                ans[ansIndex++]=j+1;
             }
         }
         return ans;
