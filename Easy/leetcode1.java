f5a84fca59c103dcc8f2bf42029cd50a645ae5fd
Yashrajsinh-Kanchva <yashrajsinhkanchva7435@gmail.com>
2025-12-04 21:05:33 +0530

    Update in Q1

--- Easy/leetcode1.java
+++ Easy/leetcode1.java
@@ -1,13 +1,20 @@
 public class leetcode1 {
     public static void main(String[] args) {
-        int[] nums={2,7,11,15};
-        int target=9;
+        Solution1 s=new Solution1();
+        int[] ans=s.twoSum(new int[]{2,7,11,15},9);
+        for(int n:ans)
+            System.out.print(n+" ");
+    }
+}
+class Solution1 {
+    public int[] twoSum(int[] nums, int target) {
         for(int i=0; i<nums.length; i++){
             for(int j=i+1; j<nums.length; j++){
                 if(nums[i]+nums[j]==target){
-                    System.out.println("["+i+","+j+"]");
+                    return new int[]{i,j};
                 }
             }
         }
+        return new int[0];
     }
 }
