883786294c875f6fd07944988b66f965677a3bd2
Yashrajsinh-Kanchva <yashrajsinhkanchva7435@gmail.com>
2026-02-14 22:53:37 +0530

    Added Q799

---
+++ Medium/lc799.java
@@ -0,0 +1,24 @@
+public class lc799 {
+    public static void main(String[] args) {
+        Solution799 s=new Solution799();
+        System.out.println(s.champagneTower(1,1,1));
+    }
+}
+class Solution799 {
+    public double champagneTower(int poured, int query_row, int query_glass) {
+        double[][] tower = new double[102][102];
+        tower[0][0] = (double) poured;
+
+        for (int r = 0; r <= query_row; r++) {
+            for (int c = 0; c <= r; c++) {
+                if (tower[r][c] > 1.0) {
+                    double excess = (tower[r][c] - 1.0) / 2.0;
+                    tower[r][c] = 1.0;
+                    tower[r + 1][c] += excess;
+                    tower[r + 1][c + 1] += excess;
+                }
+            }
+        }
+        return tower[query_row][query_glass];
+    }
+}
