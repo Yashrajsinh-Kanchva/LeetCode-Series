72b4525003fa0ff69ae2bbf721a9f811452feaca
Yashrajsinh-Kanchva <yashrajsinhkanchva7435@gmail.com>
2026-02-24 22:47:46 +0530

    Added Q1022

---
+++ Easy/lc1022.java
@@ -0,0 +1,22 @@
+public class lc1022 {
+}
+class Solution1022 {
+
+    public int sumRootToLeaf(TreeNode root) {
+        return dfs(root, 0);
+    }
+
+    private int dfs(TreeNode node, int current) {
+        if (node == null) {
+            return 0;
+        }
+
+        current = current * 2 + node.val;
+
+        if (node.left == null && node.right == null) {
+            return current;
+        }
+
+        return dfs(node.left, current) + dfs(node.right, current);
+    }
+}
